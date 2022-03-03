c3ff4a638c63f2b1d65df3b529ba3e66b947a9c9
Sergey Semushin <kezzyhko@gmail.com>
2022-03-03 09:30:23 +0300

    Show the message in the popup

--- popup.js
+++ popup.js
@@ -1,4 +1,29 @@
+
 clearButton.addEventListener("click", async () => {
 	message.innerText = ""
 	chrome.storage.local.clear()
-});
+})
+
+
+
+function updateMessage(bits) {
+	message.innerText = bits
+}
+
+
+
+chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
+
+	origin = new URL(tabs[0].url).origin
+
+	chrome.storage.local.get([origin], function(result) {
+		updateMessage(result[origin] || "")
+	});
+
+	chrome.storage.onChanged.addListener(function (changes, namespace) {
+		if (changes[origin] !== undefined) { return }
+		updateMessage(changes[origin].newValue)
+	});
+
+
+})
