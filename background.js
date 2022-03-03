3f4b04408796b19263bafa88c465d10c8e16c9f2
Sergey Semushin <kezzyhko@gmail.com>
2022-03-03 04:16:50 +0300

    Add simple headers logging

---
+++ background.js
@@ -0,0 +1,11 @@
+
+function extractCovertMessage(e) {
+  console.log(e.responseHeaders)
+}
+
+
+chrome.webRequest.onHeadersReceived.addListener(
+  extractCovertMessage,
+  { urls: ['<all_urls>'] },
+  ["responseHeaders"]
+);
